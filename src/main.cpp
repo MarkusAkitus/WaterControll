b8a76e36f454ebf085fde4f17fac17b06661190b
Marc Llorens Petit <6204845009@edu.gencat.cat>
2025-08-25 15:12:53 +0200

    translate explanations

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
-int puls = 2; // Pin del pulsador
-int LED = 10; // Pin del LED
+int puls = 2; // Push button pin
+int LED = 10; // LED pin
 
-unsigned long timeWorking = 0;   // Segundos que ha estado trabajando
-unsigned long timeOffOn = 0;     // Segundos que ha estado parado
-unsigned long prevSecond = 0;    // Para contar segundos
-unsigned long startPress = 0;    // Cuando comenzó a pulsar el botón
-bool isWorking = false;          // Estado actual
-bool ledOn = false;              // LED encendido permanentemente
+unsigned long timeWorking = 0;   // Working seconds
+unsigned long timeOffOn = 0;     // Stopped seconds
+unsigned long prevSecond = 0;    // To count seconds
+unsigned long startPress = 0;    // Start time of button press
+bool isWorking = false;          // Actual state
+bool ledOn = false;              // Permanent LED state
 
 void setup() {
   Serial.begin(9600);
@@ -21,18 +21,18 @@ void loop() {
   int pulsValue = digitalRead(puls);
   unsigned long currentMillis = millis();
 
-  if (pulsValue == LOW) { // Pulsador presionado
+  if (pulsValue == LOW) { // Push button pressed
     if (!isWorking) {
-      // Acaba de iniciar el trabajo
+      // Working state starts
       Serial.print("Temps entre parada-arranc: ");
       Serial.print(timeOffOn);
       Serial.println(" segons");
       timeOffOn = 0;
       prevSecond = currentMillis;
-      startPress = currentMillis; // Marca inicio del pulsado
+      startPress = currentMillis; // Start counting press time
     }
 
-    // Contar segundos trabajando
+    // Count working seconds
     if (currentMillis - prevSecond >= 1000) {
       prevSecond = currentMillis;
       timeWorking++;
@@ -40,14 +40,14 @@ void loop() {
 
     isWorking = true;
 
-  } else { // Pulsador suelto
+  } else { // Push button not pressed
     if (isWorking) {
-      // Se acaba de soltar
+      // Working state ends
       Serial.print("Temps treballat: ");
       Serial.print(timeWorking);
       Serial.println(" segons");
 
-      // Encender LED si pulsado ≤ 15 s
+      // Turn on LED if worked less than 15 seconds
       if ((currentMillis - startPress) <= 15000) {
         digitalWrite(LED, HIGH);
         ledOn = true;
@@ -57,13 +57,13 @@ void loop() {
       prevSecond = currentMillis;
     }
 
-    // Contar segundos en parada
+    // Count stopped seconds
     if (currentMillis - prevSecond >= 1000) {
       prevSecond = currentMillis;
       timeOffOn++;
     }
 
     isWorking = false;
-    startPress = 0; // Reiniciamos contador de pulsado
+    startPress = 0; // Teset stopped time
   }
 }
