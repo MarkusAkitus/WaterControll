f1c1be429d6cbf718fd18ed168ea768381c3e770
Marc Llorens Petit <6204845009@edu.gencat.cat>
2025-08-26 14:01:22 +0200

    make more readable the code

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,76 +1,92 @@
 // Define functions here
 #include "functions.h"
 
-void startWorking(unsigned long &timeOffOn, unsigned long &prevSecond, unsigned long &startPress, bool &isWorking) {
-    if (!isWorking) {
-        // Working state started
-        Serial.print("Temps entre parada-arranc: ");
-        Serial.print(timeOffOn);
-        Serial.println(" segons");
-        timeOffOn = 0;
-        prevSecond = millis();
-        startPress = millis();
-    }
-  
+void startWorking(unsigned long &timeOffOn, unsigned long &prevSecond, unsigned long &startPress, bool &isWorking)
+{
+  if (!isWorking)
+  {
+    // Working state started
+    Serial.print("Temps entre parada-arranc: ");
+    Serial.print(timeOffOn);
+    Serial.println(" segons");
+    timeOffOn = 0;
+    prevSecond = millis();
+    startPress = millis();
+  }
 }
 
-void workingSeconds(unsigned long &timeWorking, unsigned long &prevSecond) {
+void workingSeconds(unsigned long &timeWorking, unsigned long &prevSecond)
+{
   // Count working seconds
-  if (millis() - prevSecond >= 1000) {
+  if (millis() - prevSecond >= 1000)
+  {
     prevSecond = millis();
     timeWorking++;
   }
 }
 
-void stopWorking(unsigned long &timeWorking, int &shortPressCount) {
+void stopWorking(unsigned long &timeWorking, int &shortPressCount)
+{
   // Count short presses (≤15s)
-  if (timeWorking <= 15) {
+  if (timeWorking <= 15)
+  {
     shortPressCount++;
   }
   timeWorking = 0;
 }
 
-void shortPresses(int &shortPressCount, unsigned long &timeWorking) {
+void shortPresses(int &shortPressCount, unsigned long &timeWorking)
+{
   // Count short presses (≤15s)
-  if (timeWorking <= 15) {
+  if (timeWorking <= 15)
+  {
     shortPressCount++;
   }
 }
 
-void stoppedSeconds(unsigned long &timeOffOn, unsigned long &prevSecond) {
+void stoppedSeconds(unsigned long &timeOffOn, unsigned long &prevSecond)
+{
   // Count stopped seconds
-  if (millis() - prevSecond >= 1000) {
+  if (millis() - prevSecond >= 1000)
+  {
     prevSecond = millis();
     timeOffOn++;
   }
 }
 
-void autoreset(unsigned long &timeWorking, int &shortPressCount, unsigned long &startMinute) {
+void autoreset(unsigned long &timeWorking, int &shortPressCount, unsigned long &startMinute)
+{
   // Auto-reset after one minute
-  if (millis() - startMinute >= 60000) {
+  if (millis() - startMinute >= 60000)
+  {
     timeWorking = 0;
     shortPressCount = 0;
     startMinute = millis(); // Restart one-minute window
   }
 }
 
-void checkSigns(unsigned long &timeWorking, int &shortPressCount, bool &ledOn, int LED, int buzzer, int Do, unsigned long &lastBeep, bool &buzzerOn) {
-  if ((timeWorking > 0 || shortPressCount >= 3) && !ledOn) {
-    digitalWrite(LED, HIGH);    
-    ledOn = true;  // LED on
+void checkSigns(unsigned long &timeWorking, int &shortPressCount, bool &ledOn, int LED, int buzzer, int Do, unsigned long &lastBeep, bool &buzzerOn)
+{
+  if ((timeWorking > 0 || shortPressCount >= 3) && !ledOn)
+  {
+    digitalWrite(LED, HIGH);
+    ledOn = true; // LED on
   }
 
   // If LED = on → buzzer beeps every second
-  if (ledOn) {
+  if (ledOn)
+  {
     unsigned long currentMillis = millis();
 
-    if (buzzerOn && currentMillis - lastBeep >= 500) {
+    if (buzzerOn && currentMillis - lastBeep >= 500)
+    {
       // If it is toning during 500ms → turn off
       noTone(buzzer);
       buzzerOn = false;
       lastBeep = currentMillis;
-    } 
-    else if (!buzzerOn && currentMillis - lastBeep >= 500) {
+    }
+    else if (!buzzerOn && currentMillis - lastBeep >= 500)
+    {
       // If it is stopped durint 500 ms → turn on
       tone(buzzer, Do);
       buzzerOn = true;
@@ -79,8 +95,10 @@ void checkSigns(unsigned long &timeWorking, int &shortPressCount, bool &ledOn, i
   }
 }
 
-void reset(unsigned long &timeWorking, int &shortPressCount, bool &ledOn, int LED, int buzzer, int Do, bool &buzzerOn) {
-  if (ledOn) {
+void reset(unsigned long &timeWorking, int &shortPressCount, bool &ledOn, int LED, int buzzer, int Do, bool &buzzerOn)
+{
+  if (ledOn)
+  {
     digitalWrite(LED, LOW);
     noTone(buzzer);
     ledOn = false;
@@ -89,8 +107,10 @@ void reset(unsigned long &timeWorking, int &shortPressCount, bool &ledOn, int LE
   shortPressCount = 0;
 }
 
-void checkPuls(int pulsValue, bool &isWorking, unsigned long &startPress, unsigned long currentMillis, unsigned long &timeWorking, unsigned long &prevSecond, unsigned long &timeOffOn, int &shortPressCount, bool &ledOn, int LED) {
-  if (pulsValue == LOW) { // Button pressed
+void checkPuls(int pulsValue, bool &isWorking, unsigned long &startPress, unsigned long currentMillis, unsigned long &timeWorking, unsigned long &prevSecond, unsigned long &timeOffOn, int &shortPressCount, bool &ledOn, int LED)
+{
+  if (pulsValue == LOW)
+  { // Button pressed
     // Working state started
     startWorking(timeOffOn, prevSecond, startPress, isWorking);
 
@@ -98,9 +118,11 @@ void checkPuls(int pulsValue, bool &isWorking, unsigned long &startPress, unsign
     workingSeconds(timeWorking, prevSecond);
 
     isWorking = true;
-
-  } else { // Button released
-    if (isWorking) {
+  }
+  else
+  { // Button released
+    if (isWorking)
+    {
       // Working state ended
       stopWorking(timeWorking, shortPressCount);
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
 #include <Arduino.h>
 #include "functions.h"
 
-int puls = 2;         // Push button pin
-int resetButton = 3;  // Reset button pin
-int LED = 10;         // LED pin
-int buzzer = 9;       // Buzzer pin
-int Do = 261;         // Note frequencies
+int puls = 2;        // Push button pin
+int resetButton = 3; // Reset button pin
+int LED = 10;        // LED pin
+int buzzer = 9;      // Buzzer pin
+int Do = 261;        // Note frequencies
 
-unsigned long timeWorking = 0;    // Working seconds
-unsigned long timeOffOn = 0;      // Stopped seconds
-unsigned long prevSecond = 0;     // To count seconds
-unsigned long startPress = 0;     // Start time of button press
-unsigned long startMinute = 0;    // Start of 1-minute window
-bool isWorking = false;           // Actual state
-bool ledOn = false;               // Permanent LED state
-int shortPressCount = 0;          // Number of short presses (≤15s)
-unsigned long lastBeep = 0;       // Last state change
-bool buzzerOn = false;            // Buzzer state
+unsigned long timeWorking = 0; // Working seconds
+unsigned long timeOffOn = 0;   // Stopped seconds
+unsigned long prevSecond = 0;  // To count seconds
+unsigned long startPress = 0;  // Start time of button press
+unsigned long startMinute = 0; // Start of 1-minute window
+bool isWorking = false;        // Actual state
+bool ledOn = false;            // Permanent LED state
+int shortPressCount = 0;       // Number of short presses (≤15s)
+unsigned long lastBeep = 0;    // Last state change
+bool buzzerOn = false;         // Buzzer state
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(puls, INPUT_PULLUP);
   pinMode(resetButton, INPUT_PULLUP);
@@ -29,7 +30,8 @@ void setup() {
   startMinute = millis(); // Start one-minute window
 }
 
-void loop() {
+void loop()
+{
   int pulsValue = digitalRead(puls);
   // int buttonValue = digitalRead(resetButton);
   unsigned long currentMillis = millis();
